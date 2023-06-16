15214acc995dfbd4f3c1fab00a627422165e1de2
cchavezmx99 <carlos.chavez@99minutos.com>
2023-06-16 12:06:46 -0600

    first commit

---
+++ authConfig.js
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+const { LogLevel } = require("@azure/msal-node");
+
+/**
+ * Configuration object to be passed to MSAL instance on creation.
+ * For a full list of MSAL.js configuration parameters, visit:
+ * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-node/docs/configuration.md
+ */
+// const AAD_ENDPOINT_HOST = "Enter_the_Cloud_Instance_Id_Here"; // include the trailing slash
+const AAD_ENDPOINT_HOST = "https://login.microsoftonline.com/3ae7e579-4379-49a7-9096-813814457701/"; // include the trailing slash
+
+const msalConfig = {
+    auth: {
+        clientId: "",
+        authority: `${AAD_ENDPOINT_HOST}`,
+    },
+    system: {
+        loggerOptions: {
+            loggerCallback(loglevel, message, containsPii) {                
+                console.log(message);
+            },
+            piiLoggingEnabled: false,
+            logLevel: LogLevel.Verbose,
+        },
+    },
+};
+
+/**
+ * Add here the endpoints and scopes when obtaining an access token for protected web APIs. For more information, see:
+ * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/resources-and-scopes.md
+ */
+// const GRAPH_ENDPOINT_HOST = "Enter_the_Graph_Endpoint_Here"; // include the trailing slash
+const GRAPH_ENDPOINT_HOST = ""; // include the trailing slash
+
+const protectedResources = {
+    graphMe: {
+        endpoint: `${GRAPH_ENDPOINT_HOST}/v1.0/me`,
+        scopes: ["User.Read"],
+    }
+};
+
+
+module.exports = {
+    msalConfig: msalConfig,
+    protectedResources: protectedResources,
+};
